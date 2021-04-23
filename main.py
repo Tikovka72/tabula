8f0e0b505eb7794c073c971ecfff49753fa55dc2
Tikovka72 <zeytunyan2004@mail.ru>
2021-04-23 23:14:27 +0500

    rename files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets
 
 import sys
 
-from core import Core
+from main_window import Core
 
 from managers.mouse_manager import MouseManager
 from managers.widget_manager import WidgetManager
